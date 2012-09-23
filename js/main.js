59ebec75498a096387dfc8c821f6bd269586918a
Terry Moore <terry.moore.ii@gmail.com>
2012-09-22 20:18:55 -0400

    Updated style and knockout bindings.  Added default search when page loads Still need to get search hashtags when clicking on the hashtag.

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@
 var Tweet = function(createdAt, fromUser, fromUserName, location, profileImgUrl, sourceUrl, text){
 	this.createdAt = createdAt;
 	this.fromUser = fromUser;
+	this.fromUserLink = 'http://twitter.com/#!/' + fromUser;
 	this.fromUserName = fromUserName;
 	this.location = location;
 	this.profileImgUrl = profileImgUrl;
-	this.sourceUrl = sourceUrl;
+	this.sourceUrl = '<a class="linkToTweet" href="' + sourceUrl + '">Link</a>';
 	this.text = text;
+
 }
 
 var TwitterViewModel = function(){
@@ -15,31 +17,53 @@ var TwitterViewModel = function(){
 
 	self.search = ko.observable();
 	self.tweets = ko.observableArray([]);
+	self.hashSearch = ko.observable();
 
-	var init = function(){
-
+	self.init = function(){
+		self.getTweets('cardinalNow');
 	};
 
 	self.search.subscribe(function(form) {
-		self.getTweets($(searchValue).val())	
+		self.getTweets($(searchValue).val());
+	});
+	
+	self.sanitize = function(search){
+		return search.replace('#', '%23')
+	};
+
+	self.hashSearch.subscribe(function(search){
+		self.getTweets(search);
+		console.log(search);
+		$('#searchValue').value(search);
 	});
 
 	self.getTweets = function(search){
+
+		search = self.sanitize(search);
 		var url = 'http://search.twitter.com/search.json?callback=?&q=' + search; 
  		self.clearTweets();
 		$.getJSON(url, function(json){
 			$.each(json.results, function(k, tweet){
-				self.tweets.push( new Tweet(tweet.created_at, 
+				self.tweets.push( new Tweet(tweet.created_at.split('+')[0], 
 										tweet.from_user, 
 										tweet.from_user_name, 
 										tweet.geo, 
-										tweet.profile_image_url, 
+										tweet.profile_image_url,  
 										tweet.source,
-										tweet.text));				
+										self.hyperLinks(tweet.text)));				
 			});
-		console.log(self.tweets());
 		});
 	};
+
+	self.hyperLinks = function replaceURLWithHTMLLinks(text) {
+	    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+	    text = text.replace(exp,"<a href='$1'>$1</a>"); 
+
+		var exp = /(#[A-Za-z0-9]*)/ig;
+	    text = text.replace(exp,"<a href='#' data-bind='click: viewModel.hashSearch.bind($1)'>$1</a>"); 
+
+	    return text;
+	}	
 
 	self.clearTweets = function(){
 		self.tweets([]);
@@ -49,5 +73,6 @@ var TwitterViewModel = function(){
 
 
 var viewModel = new TwitterViewModel();
-
 ko.applyBindings(viewModel);
+
+viewModel.init();
